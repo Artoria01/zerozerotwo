76a3f61f7405cc9f6f12173d07240220c07341de
Artoria01 <79462135+Artoria01@users.noreply.github.com>
2021-07-05 17:55:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+  client.user.setActivity(`${PREFIX}help et ${PREFIX}play`, { type: "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
